ebd7393ed0a68cf589f08bb93a0fa7830781e9a0
diazy2003 <fidel.diaz@utp.ac.pa>
2022-05-26 19:23:56 -0500

    Cambio 2

--- src/App.java
+++ src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Primer cambio");
+        System.out.println("Segundo cambio");
     }
 }
