d99d9309702c3538d2b3b643e95c4e8029ad06e6
diazy2003 <90580124+diazy2003@users.noreply.github.com>
2022-05-26 19:55:36 -0500

    Update App.java

--- src/App.java
+++ src/App.java
@@ -4,6 +4,5 @@ public class App {
         System.out.println("Primer cambio");
         System.out.println("Segundo cambio");
         System.out.println("Tercer cambio");
-        System.out.println("Cuarto cambio");
     }
 }
